f1887410d559d7e43be990f7230b942f92f403f6
Your Name <you@example.com>
2020-10-08 19:31:37 +0530

    [shashi] add . Get index to block move

--- tictoctoegame/TicTocToeGame.java
+++ tictoctoegame/TicTocToeGame.java
@@ -129,6 +129,31 @@ public class TicTocToeGame {
 			return "turn";
 	}
 
+	/**
+	 * UC9
+	 * 
+	 * @param letter
+	 * @return
+	 */
+	public static int getIndexToBlockMove(char letter) {
+		int index;
+		char dummyLetter = 'O';
+		if (letter == dummyLetter)
+			dummyLetter = 'X';
+
+		for (index = 1; index <= 9; index++) {
+			char[] dummyBoard = board;
+			if (dummyBoard[index] == ' ') {
+				dummyBoard[index] = dummyLetter;
+				String status = checkStatus(dummyLetter);
+				if (status.equals("win"))
+					return index;
+			} else
+				continue;
+		}
+		return 0;
+	}
+
 	/*
 	 * main
 	 */
@@ -145,7 +170,8 @@ public class TicTocToeGame {
 		showBoard();
 		int position = desiredLocation();
 		makeMove(position, userChoice);
-		String whoseChanceToPlay = checkFirstMove();
-		String status = checkStatus(userChoice);
+		String firstChanceToPlay = checkFirstMove();
+		String gameStatusBeforeMove = checkStatus(userChoice);
+		int blockingIndex = getIndexToBlockMove(userChoice);
 	}
 }
